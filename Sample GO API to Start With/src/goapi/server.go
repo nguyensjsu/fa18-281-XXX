3ff7ac658ca32b73c50b6fbaa868d6442b438b71
Shubham Sawant <shubham.sawant@sjsu.edu>
2018-11-12 00:40:32 -0800

    Updated the sample GOAPI - Implemented Upsate and Delete methods

--- Sample GO API to Start With/src/goapi/server.go
+++ Sample GO API to Start With/src/goapi/server.go
@@ -8,13 +8,14 @@ import (
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/rs/xid"
 	"github.com/unrolled/render"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
 // MongoDB Config
-var mongodb_server = "mongodb://admin:cmpe281@54.188.166.200,34.210.2.194,34.217.108.75,52.88.34.228,54.202.39.158"
+var mongodb_server = "mongodb://admin:cmpe281@34.221.158.202,54.185.27.49,18.237.78.8,54.186.122.30,34.216.148.220"
 
 //var mongodb_server1 string
 //var mongodb_server2 string
@@ -50,6 +51,8 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/users/{email}", userHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/users", userSignUpHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/users/{email}", deleteUserHandler(formatter)).Methods("DELETE")
+	mx.HandleFunc("/users", updateUserHandler(formatter)).Methods("PUT")
 
 }
 
@@ -60,11 +63,10 @@ func pingHandler(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
-// API  Handler --------------- GET ------------------
+// API  Handler --------------- Get the user info (GET) ------------------
 func userHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 
-		//var m test
 		vars := mux.Vars(req)
 		email := vars["email"]
 
@@ -76,30 +78,38 @@ func userHandler(formatter *render.Render) http.HandlerFunc {
 		defer session.Close()
 		session.SetMode(mgo.PrimaryPreferred, true)
 		c := session.DB(mongodb_database).C(mongodb_collection)
+
+		fmt.Println(email)
 		var result bson.M
 		err = c.Find(bson.M{"Email": email}).One(&result)
-		if err != nil {
-			log.Fatal(err)
-		}
+
 		fmt.Println("Result :", result)
-		formatter.JSON(w, http.StatusOK, result)
+		if result == nil {
+			var noUser user
+			noUser.Message = "User does not exist"
+			formatter.JSON(w, http.StatusOK, noUser)
+		} else {
+			formatter.JSON(w, http.StatusOK, result)
+		}
 	}
 }
 
-// API  Handler --------------- POST ------------------
+// API  Handler --------------- Register a new user (POST) ------------------
 func userSignUpHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 
-		var m user
-		_ = json.NewDecoder(req.Body).Decode(&m)
-
-		fmt.Println(m)
 		session, err := mgo.Dial(mongodb_server)
 		defer session.Close()
 		session.SetMode(mgo.PrimaryPreferred, true)
 		c := session.DB(mongodb_database).C(mongodb_collection)
 
 		var result bson.M
+		var newUser user
+		_ = json.NewDecoder(req.Body).Decode(&newUser)
+
+		uuidForNewUser := xid.New()
+
+		newUser.UserID = uuidForNewUser.String()
 
 		fmt.Println("Register new user ")
 
@@ -108,16 +118,71 @@ func userSignUpHandler(formatter *render.Render) http.HandlerFunc {
 		}
 		fmt.Println("Connected to Database")
 
-		query := bson.M{"Email": m.Email, "Password": m.Password}
+		query := bson.M{"UserID": newUser.UserID, "Name": newUser.Name, "Email": newUser.Email, "Password": newUser.Password}
 		err = c.Insert(query)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		err = c.Find(bson.M{"Email": m.Email}).One(&result)
+		err = c.Find(bson.M{"UserID": newUser.UserID}).One(&result)
 		if err != nil {
 			log.Fatal(err)
 		}
 		formatter.JSON(w, http.StatusOK, result)
+	}
+}
+
+// API  Handler --------------- Delete a user (DELETE) ------------------
+func deleteUserHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		vars := mux.Vars(req)
+		email := vars["email"]
+
+		session, err := mgo.Dial(mongodb_server)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer session.Close()
+		session.SetMode(mgo.PrimaryPreferred, true)
+		c := session.DB(mongodb_database).C(mongodb_collection)
+
+		c.Remove(bson.M{"Email": email})
+		var deletedUser user
+		deletedUser.Email = email
+		deletedUser.Message = "User has been deleted"
+		formatter.JSON(w, http.StatusOK, deletedUser)
+	}
+}
+
+//TODO
+func updateUserHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		session, err := mgo.Dial(mongodb_server)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer session.Close()
+		session.SetMode(mgo.PrimaryPreferred, true)
+		c := session.DB(mongodb_database).C(mongodb_collection)
+
+		var newUser user
+		_ = json.NewDecoder(req.Body).Decode(&newUser)
+
+		var result bson.M
+		err = c.Find(bson.M{"Email": newUser.Email}).One(&result)
+
+		err = c.Update(bson.M{"Email": newUser.Email}, newUser)
+
+		var updatedUser user
+		updatedUser.Email = newUser.Email
+		updatedUser.Message = "User details have been successfully updated."
+		formatter.JSON(w, http.StatusOK, updatedUser)
+
 	}
 }
