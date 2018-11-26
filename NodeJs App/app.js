58c7536f33fdfc5c2d49245cb9a8db2249b83bce
Shubham Sawant <shubham.sawant@sjsu.edu>
2018-11-25 20:48:47 -0800

    Integrated product_catalog API in nodeJS frontend

--- NodeJs App/app.js
+++ NodeJs App/app.js
@@ -10,11 +10,12 @@ var Client = require('node-rest-client').Client;
 var secret = require('./config/secret');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
+var flash = require('express-flash');
 
 var app = express();
 
 app.use(session({
-	secret: 'mysupersecret',
+	secret: 'guessTheSecret',
 	resave: false,
 	saveUninitialiazed: false,
 	cookie: { maxAge: 180 * 60 * 1000} //in milliseconds
@@ -28,8 +29,14 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 var userloginServer = "http://localhost:5000/"
+var productCatalogueServer = "http://localhost:5001/"
 
 var isLoggedIn = false;
+var cartQuantity = 0;
+var cart = Object();
+
+cart.products = [];
+cart.total = 0;
 
 app.get('/signin', function(request, response) {
 	/*if (request.session.userid) {
@@ -43,7 +50,26 @@ app.get('/signup', function(request, response) {
 });
 
 app.post('/signin', function(request, response) {
+	var emailID = request.body.email;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", userloginServer+ "users/" +emailID);
+	xmlhttp.setRequestHeader("Content-Type", "application/json");
+	xmlhttp.send();
 
+	xmlhttp.onreadystatechange = function()
+	{
+		if (this.readyState === 4 && this.status === 200) {
+			var responseText = JSON.parse(this.responseText);
+
+			if(responseText.Email == emailID) {
+				isLoggedIn = true;
+				response.redirect("/products");
+			}
+			else {
+				response.redirect("/signin");
+			}
+		}
+	}
 });
 
 app.post('/signup', function(request, response) {
@@ -57,19 +83,21 @@ app.post('/signup', function(request, response) {
 	var jsonToSend = {
 		"Name": request.body.name,
 		"Email":  request.body.email,
+		"Address": request.body.address,
 		"Password": request.body.password
 	};
 
 	xmlhttp.send(JSON.stringify(jsonToSend));
 
-	xmlhttp.onreadystatechange = function() {
+	xmlhttp.onreadystatechange = function()
+	{
 		if (this.readyState === 4 && this.status === 200) {
-			request.session.userid = request.body.email;
-			response.redirect("/")
+			//request.session.userid = request.body.email;
+			response.redirect("/signin")
 		}
 		else if (this.readyState === 4 && this.status !== 200) {
 			console.log("Cannot post to user database");
-			response.redirect("/");
+			response.redirect("/signup");
 		}
 	}
 });
@@ -82,7 +110,53 @@ app.get('/', function(request, response){
 		isLoggedIn = false;
 	}
 
-	response.render('user/login', {login:isLoggedIn, cartQuantity: 0});
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", productCatalogueServer+ "products");
+	xmlhttp.setRequestHeader("Content-Type", "application/json");
+	xmlhttp.send();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+			var products_array = JSON.parse(this.responseText);
+			console.log(products_array);
+			response.render('./main/catalog', {products: products_array, login: isLoggedIn, cartQuantity: 0});
+		}
+	}
+	//response.render('user/login', {login:isLoggedIn, cartQuantity: 0});
+});
+
+app.get('/products', function(request, response) {
+	var xmlhttp1 = new XMLHttpRequest();
+	xmlhttp1.open("GET", productCatalogueServer+ "products");
+	xmlhttp1.setRequestHeader("Content-Type", "application/json");
+	xmlhttp1.send();
+
+	xmlhttp1.onreadystatechange = function()
+	{
+			if (this.readyState === 4 && this.status === 200) {
+				var products_array = JSON.parse(this.responseText);
+				response.render('./main/catalog', {products: products_array, login: isLoggedIn, cartQuantity: 0});
+			}
+	}
+});
+
+app.get('/products/:id', function(request, response) {
+	var productId = request.params["id"];
+	var xmlhttp1 = new XMLHttpRequest();
+	xmlhttp1.open("GET", productCatalogueServer+ "products/" + productId);
+	xmlhttp1.setRequestHeader("Content-Type", "application/json");
+	xmlhttp1.send();
+
+	xmlhttp1.onreadystatechange = function()
+	{
+		if (this.readyState === 4 && this.status === 200) {
+			var product = JSON.parse(this.responseText);
+			response.render('./main/product', {product: product, login: isLoggedIn, cartQuantity: 0});
+		}
+	}
+});
+
+app.get('/logout', function(request, response) {
+
 });
 
 app.listen(secret.port, function (err) {
