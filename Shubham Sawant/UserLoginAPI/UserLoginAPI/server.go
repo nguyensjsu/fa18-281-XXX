c0922601cb1ef796122225c4b0dc8ce10051f590
Shubham Sawant <shubham.sawant@sjsu.edu>
2018-12-02 13:17:34 -0800

    Removed hardcoded server values from GoAPI

--- Shubham Sawant/UserLoginAPI/UserLoginAPI/server.go
+++ Shubham Sawant/UserLoginAPI/UserLoginAPI/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
@@ -13,29 +14,15 @@ import (
 )
 
 // MongoDB Config
-var mongodb_server = "mongodb://admin:cmpe281@54.202.14.83,34.214.186.52,34.221.233.248,54.149.131.94,54.188.133.250"
-
-//var mongodb_server1 string
-//var mongodb_server2 string
-//var redis_server string
-
-//="mongodb://54.67.13.87:27017,54.67.106.101:27017,13.57.39.192:27017,54.153.26.217://27017,52.53.154.42:27017"
-
-var mongodb_database = "TeamProject"
-var mongodb_collection = "user"
+var mongodb_server = os.Getenv("MONGO_SERVERS")
+var mongodb_database = os.Getenv("MONGO_DB")
+var mongodb_collection = os.Getenv("MONGO_COLLECTION")
 
 // NewServer configures and returns a Server.
 func NewServer() *negroni.Negroni {
 	formatter := render.New(render.Options{
 		IndentJSON: true,
 	})
-
-	//mongodb_server = os.Getenv("MONGO1")
-	//mongodb_server1 = os.Getenv("MONGO2")
-	//mongodb_server2 = os.Getenv("MONGO3")
-	//mongodb_database = os.Getenv("MONGO_DB")
-	//mongodb_collection = os.Getenv("MONGO_COLLECTION")
-	//redis_server = os.Getenv("REDIS")
 
 	n := negroni.Classic()
 	mx := mux.NewRouter()
